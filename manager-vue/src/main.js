b0cb3b238b240aa89c08cfd789d51e98a9bdff47
qwer <2014567412@qq.com>
2025-11-20 15:58:40 +0800

    Vue2

--- manager-vue/src/main.js
+++ manager-vue/src/main.js
@@ -1,7 +1,12 @@
 import Vue from 'vue'
 import App from './App.vue'
+import settings from "@/settings";                                   // 导入配置文件
+import ElementUI from 'element-ui';                                  // 导入element-ui组件
+import 'element-ui/lib/theme-chalk/index.css';                       // 导入element-ui组件样式表
 
 Vue.config.productionTip = false
+Vue.prototype.$settings = settings;
+Vue.use(ElementUI);
 
 new Vue({
   render: h => h(App),
