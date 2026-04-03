dd163a1161aed088af4e6fc33372135dfcf16a10
spidermanehe <2014567412@qq.com>
2026-04-03 13:24:05 +0800

    update

--- Esp32/main.py
+++ Esp32/main.py
@@ -1,12 +1,18 @@
 from machine import Pin
 import time
 
-led = Pin(2, Pin.OUT)                            # 板载 LED
+led = Pin(2, Pin.OUT, value=0)                            
+led2 = Pin(19,Pin.OUT, value=0)
+
+led2.value(1)
+print("蜂鸣器输出")
+time.sleep(5)
+led2.value(0)
 
 while True:
     led.value(1)
     print("灯亮")
-    time.sleep(1)
+    time.sleep(5)
     led.value(0)
     print("灯灭")
-    time.sleep(1)
+    time.sleep(2)
